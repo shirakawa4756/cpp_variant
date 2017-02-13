47a35f889ebdf3887a450eb8799a964ff5cfb9e9
Akihiro KAMEMOTO <a-kamemoto@kuze.com>
2017-02-13 19:48:00 +0900

    modify: main.cpp: テストコードの更新

--- msvc12/main.cpp
+++ msvc12/main.cpp
@@ -12,8 +12,17 @@ int main()
 
     variant::Variant values[] = {
             false, true, 0, 1, -10, 10, 0.0, 1.0, -10.1, 10.1,
+            INT64_MAX, INT64_MIN,
+            std::numeric_limits<double>::max(),
+            std::numeric_limits<double>::min(),
             "false", "true", "0", "1", "-10", "10", "0.0", "1.0",
-            "-10.2", "10.2", "anything", "Convert to?", "日本語大丈夫?" };
+            "-10.2", "10.2", "100$", "-100$", "100.5$", "-100.5$",
+            "9223372036854775807", "-9223372036854775806",
+            "9223372036854775808", "-9223372036854775807"
+            "1.79769e+308", " 2.22507e-308",
+            "1.79769e+309", " 2.22507e-309",
+            "anything", "Convert to?",
+            "日本語大丈夫?" };
 
     std::cout << "□ Convert to Double! (tryCast)" << std::endl;
     for (const auto &v : values) {
@@ -28,11 +37,13 @@ int main()
             std::cout << "Attempted: " << v << std::endl;
         }
     }
+    std::cout << std::endl;
 
     std::cout << "□ Convert to wchar_t." << std::endl;
     for (const auto &v : values) {
         std::wcout << v << std::endl;
     }
+    std::cout << std::endl;
 
     std::cout << "□ Convert to various types using cast." << std::endl;
     for (const auto &v : values) {
@@ -49,6 +60,7 @@ int main()
                   << d   << ", "
                   << s   << std::endl;
     }
+    std::cout << std::endl;
 
     return 0;
 }
